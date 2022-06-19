91f879de7ff6844f3c58a587555c4de18c7175df
Oscar Hernandez <oscarhernandezmex@gmail.com>
2022-06-18 21:46:25 -0700

    components folders updated utils added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./About.css";
 
 function About() {
   return (
     <section>
       <h1 id="about">About Me</h1>
+      <p>
+        I am a father of 3 amazing kids. Currently attending a coding boot-camp
+        at Arizona University. Looking to switch into a career in coding. I
+        believe it is an ever evolving career path with infinite possibilities.
+      </p>
     </section>
   );
 }
---
+++ src/components/Contact/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
+import "./Contact.css";
+
+function Contact() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const { name, email, message } = formState;
+
+  const handleChange = (event) => {
+    // todo if error checking do it here
+    if (event.target.name === "email") {
+      const isValid = validateEmail(event.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        if (!event.target.value.length) {
+          setErrorMessage(`${event.target.name} is required.`);
+        } else {
+          setErrorMessage("");
+        }
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [event.target.name]: event.target.value });
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // todo error checking if needed
+    console.log("submitted");
+  };
+
+  return (
+    <section>
+      <h1 id="contact">Contact Me</h1>
+      <form id="todo" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address: </label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message: </label>
+          <input type="text" name="message" defaultValue={name} />
+          <textarea name="" rows="" defaultValue="" onChange="" />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text"></p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
