de72c253fdc809e39c8f0cc04334544ea5a916b1
Oscar Hernandez <oscarhernandezmex@gmail.com>
2022-06-19 19:09:38 -0700

    About updated

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,9 +6,11 @@ function About() {
     <section>
       <h1 id="about">About Me</h1>
       <p>
-        I am a father of 3 amazing kids. Currently attending a coding boot-camp
-        at Arizona University. Looking to switch into a career in coding. I
-        believe it is an ever evolving career path with infinite possibilities.
+        I am a father of 3 amazing kids, I love my family beyond words.
+        Currently attending a coding boot-camp at Arizona University. Looking to
+        switch into a career in coding. I believe it is an ever evolving career
+        path with infinite possibilities. Excited to start my new journey in a
+        rewarding and growing field.
       </p>
     </section>
   );
